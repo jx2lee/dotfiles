63f2b7278b7fe5e15506ab0f0238dffdbcecd931
jx2lee <dev.jaejun.lee.1991@gmail.com>
2026-02-03 23:29:54 +0900

    add tmux/use --install-packages flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,14 @@
 set -e
 REPO_ROOT=$(cd "$(dirname "$0")" && pwd)
 
-stow --dir $REPO_ROOT --target $HOME zsh vim git
+stow --dir $REPO_ROOT --target $HOME zsh vim git tmux
 
-if command -v brew &>/dev/null; then
-  brew bundle --no-upgrade --file=brew/Brewfile
-fi
+if [[ "$1" == "--install-packages" ]]; then
+  if command -v brew &>/dev/null; then
+    brew bundle --no-upgrade --file=brew/Brewfile
+  fi
 
-if command -v code &>/dev/null; then
-  xargs -L 1 code --install-extension < vscode/vscode-extensions.txt
+  if command -v code &>/dev/null; then
+    xargs -L 1 code --install-extension < vscode/vscode-extensions.txt
+  fi
 fi
